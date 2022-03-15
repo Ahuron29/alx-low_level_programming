1c0fc5deee0b972b9a7647bc08543be84db1b843
Ahuron29 <ahurone@gmail.com>
2022-03-15 15:09:47 -0700

    Lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Description: Uses the holberton header file, that brings in the _putchar
+ *  funtction.
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+char ch;
+
+ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+++ch;
+}
+_putchar('\n');
+}

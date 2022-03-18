a6276fd92f4792015143af4ae24f3a1c70ad459b
Ahuron29 <ahurone@gmail.com>
2022-03-17 17:11:39 -0700

    Print straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints a line with the length being the input
+ * @n: the length of the line
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; ++i)
+_putchar('_');
+_putchar('\n');
+}

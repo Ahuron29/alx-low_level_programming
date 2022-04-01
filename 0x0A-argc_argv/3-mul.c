269b27f5ab4d3fa2365f51176d5279ce682dd851
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-04-01 23:16:15 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,20 +9,19 @@
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
-	{
+{
 
-	 int total;
+int total;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
 
-	total = (int) *argv[1] * (int) *argv[2];
+total = (int) *argv[1] * (int) *argv[2];
 
-	printf("%d\n", total);
+printf("%d\n", total);
 
-	return (0);
-
-	}
+return (0);
+}

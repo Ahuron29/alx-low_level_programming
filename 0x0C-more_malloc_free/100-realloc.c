ff898480939ed8a873ca9811c12afce5adb89773
Ahuron29 <ahurone@gmail.com>
2022-04-07 14:37:24 -0700

    Memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,29 +8,29 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  char *p, *castptr;
-  unsigned int i;
+char *p, *castptr;
+unsigned int i;
 
-  if (ptr != NULL && new_size == 0)
-    {
-      free(ptr);
-      return (NULL);
-    }
-  if (new_size == old_size)
-    return (ptr);
-  if (ptr == NULL)
-    {
-      p = malloc(new_size);
-      return (p);
-    }
-  if (new_size > old_size)
-    {
-      p = malloc(new_size * sizeof(char));
-      castptr = ptr;
-      for (i = 0; i < old_size; i++)
-	p[i] = castptr[i];
-      free(ptr);
-      return (p);
-    }
-  return (ptr);
+if (ptr != NULL && new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+if (new_size == old_size)
+return (ptr);
+if (ptr == NULL)
+{
+p = malloc(new_size);
+return (p);
+}
+if (new_size > old_size)
+{
+p = malloc(new_size * sizeof(char));
+castptr = ptr;
+for (i = 0; i < old_size; i++)
+p[i] = castptr[i];
+free(ptr);
+return (p);
+}
+return (ptr);
 }

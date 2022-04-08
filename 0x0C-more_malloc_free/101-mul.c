080485c17ad7d5cc739ebd02c381b33c15734b52
Ahuron29 <ahurone@gmail.com>
2022-04-07 18:20:50 -0700

    Two Positive number

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,20 +10,20 @@
  */
 void _print(char *str, int l)
 {
-  int i, j;
+int i, j;
 
-  i = j = 0;
-  while (i < l)
-    {
-      if (str[i] != '0')
-	j = 1;
-      if (j || i == l - 1)
-	_putchar(str[i]);
-      i++;
-    }
+i = j = 0;
+while (i < l)
+{
+if (str[i] != '0')
+j = 1;
+if (j || i == l - 1)
+_putchar(str[i]);
+i++;
+}
 
-  _putchar('\n');
-  free(str);
+_putchar('\n');
+free(str);
 }
 
 /**
@@ -38,28 +38,28 @@ void _print(char *str, int l)
  */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-  int j, k, mul, mulrem, add, addrem;
+int j, k, mul, mulrem, add, addrem;
 
-  mulrem = addrem = 0;
-  for (j = num_index, k = dest_index; j >= 0; j--, k--)
-    {
-      mul = (n - '0') * (num[j] - '0') + mulrem;
-      mulrem = mul / 10;
-      add = (dest[k] - '0') + (mul % 10) + addrem;
-      addrem = add / 10;
-      dest[k] = add % 10 + '0';
-    }
-  for (addrem += mulrem; k >= 0 && addrem; k--)
-    {
-      add = (dest[k] - '0') + addrem;
-      addrem = add / 10;
-      dest[k] = add % 10 + '0';
-    }
-  if (addrem)
-    {
-      return (NULL);
-    }
-  return (dest);
+mulrem = addrem = 0;
+for (j = num_index, k = dest_index; j >= 0; j--, k--)
+{
+mul = (n - '0') * (num[j] - '0') + mulrem;
+mulrem = mul / 10;
+add = (dest[k] - '0') + (mul % 10) + addrem;
+addrem = add / 10;
+dest[k] = add % 10 + '0';
+}
+for (addrem += mulrem; k >= 0 && addrem; k--)
+{
+add = (dest[k] - '0') + addrem;
+addrem = add / 10;
+dest[k] = add % 10 + '0';
+}
+if (addrem)
+{
+return (NULL);
+}
+return (dest);
 }
 /**
  * check_for_digits - checks the arguments to ensure they are digits
@@ -69,17 +69,17 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
  */
 int check_for_digits(char **av)
 {
-  int i, j;
+int i, j;
 
-  for (i = 1; i < 3; i++)
-    {
-      for (j = 0; av[i][j]; j++)
-	{
-	  if (av[i][j] < '0' || av[i][j] > '9')
-	    return (1);
-	}
-    }
-  return (0);
+for (i = 1; i < 3; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+if (av[i][j] < '0' || av[i][j] > '9')
+return (1);
+}
+}
+return (0);
 }
 
 /**
@@ -91,11 +91,11 @@ int check_for_digits(char **av)
  */
 void init(char *str, int l)
 {
-  int i;
+int i;
 
-  for (i = 0; i < l; i++)
-    str[i] = '0';
-  str[i] = '\0';
+for (i = 0; i < l; i++)
+str[i] = '0';
+str[i] = '\0';
 }
 
 /**
@@ -107,41 +107,41 @@ void init(char *str, int l)
  */
 int main(int argc, char *argv[])
 {
-  int l1, l2, ln, ti, i;
-  char *a;
-  char *t;
-  char e[] = "Error\n";
+int l1, l2, ln, ti, i;
+char *a;
+char *t;
+char e[] = "Error\n";
 
-  if (argc != 3 || check_for_digits(argv))
-    {
-      for (ti = 0; e[ti]; ti++)
-	_putchar(e[ti]);
-      exit(98);
-    }
-  for (l1 = 0; argv[1][l1]; l1++)
-    ;
-  for (l2 = 0; argv[2][l2]; l2++)
-    ;
-  ln = l1 + l2 + 1;
-  a = malloc(ln * sizeof(char));
-  if (a == NULL)
-    {
-      for (ti = 0; e[ti]; ti++)
-	_putchar(e[ti]);
-      exit(98);
-    }
-  init(a, ln - 1);
-  for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-    {
-      t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-      if (t == NULL)
-	{
-	  for (ti = 0; e[ti]; ti++)
-	    _putchar(e[ti]);
-	  free(a);
-	  exit(98);
-	}
-    }
-  _print(a, ln - 1);
-  return (0);
+if (argc != 3 || check_for_digits(argv))
+{
+for (ti = 0; e[ti]; ti++)
+_putchar(e[ti]);
+exit(98);
+}
+for (l1 = 0; argv[1][l1]; l1++)
+;
+for (l2 = 0; argv[2][l2]; l2++)
+;
+ln = l1 + l2 + 1;
+a = malloc(ln * sizeof(char));
+if (a == NULL)
+{
+for (ti = 0; e[ti]; ti++)
+_putchar(e[ti]);
+exit(98);
+}
+init(a, ln - 1);
+for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+{
+t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+if (t == NULL)
+{
+for (ti = 0; e[ti]; ti++)
+_putchar(e[ti]);
+free(a);
+exit(98);
+}
+}
+_print(a, ln - 1);
+return (0);
 }

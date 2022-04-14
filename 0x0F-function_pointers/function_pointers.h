0c4bc52613673a8dee165afc387f194a761872d2
Ahuron29 <ahurone@gmail.com>
2022-04-13 20:08:33 -0700

    Function Prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef ASSIGN_0x0E
-#define ASSIGN_0X0E
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 
 #include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

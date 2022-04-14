abeda8c8ddc79184c5f133e131ae7b09d7efc8f5
Ahuron29 <ahurone@gmail.com>
2022-04-13 19:03:46 -0700

    My name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
- * @name: input string
- * @f: function pointer casted to char pointer
+ * @name: name to print
+ * @f: function to print with
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }

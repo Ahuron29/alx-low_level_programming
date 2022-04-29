16bc059a99c5177cd6601ee735aaa06777a6c30c
Ahuron29 <ahurone@gmail.com>
2022-04-28 21:53:55 -0700

    Binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,13 +9,13 @@
  */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
-  unsigned long int num;
-  unsigned int i;
+unsigned long int num;
+unsigned int i;
 
-  num = 1;
-  for (i = 1; i <= power; i++)
-    num *= base;
-  return (num);
+num = 1;
+for (i = 1; i <= power; i++)
+num *= base;
+return (num);
 }
 
 /**

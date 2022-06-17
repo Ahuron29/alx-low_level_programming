6e073f90f9e8a9394e170afc0756223f4b689a39
Ahuron29 <ahurone@gmail.com>
2022-06-16 17:55:43 -0700

    Seperated files

---
+++ 0x18-dynamic_libraries/main/6_abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of number.
+ * @n: is the number to be converted to its absolute value.
+ *
+ * Return: the number converted to absolute value.
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (n * -1);
+}
---
+++ 0x18-dynamic_libraries/main/operations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "operators.h"
+/**
+ * add the two integers
+ * @a: first int
+ * @b: second int
+ * Return result
+ */
+int add(int a, int b)
+{
+  return (a + b);
+}
+/**
+ * subtract the two integers
+ * @a: first int
+ * @b: second int
+ * Return the result
+ */
+int sub(int a, int b)
+{
+  return (a - b);
+}
+/**
+ * multiply the two integers
+ * @a: first int
+ * @b: second int
+ * Return the result
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
+/**
+ * divide the two integers
+ * @a: first int
+ * @b: second int
+ * Return the result
+ */
+int div(int a, int b)
+{
+  return (a / b);
+}
+/**
+ * Modulus of the two integers
+ * @a: first int
+ * @v: second int
+ * Return the result
+ */
+int mod(int a, int b)
+{
+  return (a % b);
+}

aeefc3a1474b36718751d7dbe05b2fb83aff10a4
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-07-01 04:32:31 +0000

    Hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,22 +7,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *table = malloc(sizeof(hash_table_t));
+        hash_table_t *table = malloc(sizeof(hash_table_t));
 
-  if (size <= 0)
-    {
-      return (NULL);
-    }
-  if (table == NULL)
-    {
-      return (NULL);
-    }
-  table->size = size;
-  table->array = calloc(size, sizeof(hash_node_t *));
-  if (table->array == NULL)
-    {
-      free(table);
-      return (NULL);
-    }
-  return (table);
+        if (size <= 0)
+        {
+              return (NULL);
+        }
+        if (table == NULL)
+        {
+              return (NULL);
+        }
+        table->size = size;
+        table->array = calloc(size, sizeof(hash_node_t *));
+        if (table->array == NULL)
+        {
+              free(table);
+              return (NULL);
+        }
+        return (table);
 }

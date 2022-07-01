ebea9087df6ba9d2c17113611c6309e576787b28
Ahuron29 <ahurone@gmail.com>
2022-06-30 20:42:07 -0700

    Associated with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * *hash_table_get - define function
+ * @ht: hash table to lookup
+ * @key: key to lookup in the hash table
+ * Return: NULL on error, the value of the key on success
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  unsigned long int index;
+  hash_node_t *tmp;
+
+  index = 0;
+  if (!ht || !key)
+    return (NULL);
+  index = key_index((const unsigned char *)key, ht->size);
+  tmp = ht->array[index];
+  if (tmp)
+    {
+      while (tmp && strncmp(tmp->key, key, strlen(key)) != 0)
+	tmp = tmp->next;
+      return (tmp != NULL ? tmp->value : NULL);
+    }
+  return (NULL);
+}

c2cb74a021a860f56a4b024e26785289e9f22347
Ahuron29 <ahurone@gmail.com>
2022-06-30 20:57:12 -0700

    Prints hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print the hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+  unsigned long int i, j;
+  hash_node_t *tmp;
+
+  i = j = 0;
+  if (!ht)
+    return;
+  putchar('{');
+  for (i = j = 0; i < ht->size; i++)
+    {
+      if (ht->array[i] != NULL)
+	{
+	  tmp = ht->array[i];
+	  while (tmp)
+	    {
+	      printf("%s'%s': '%s'", j == 0 ? "" : ", ", tmp->key, tmp->value), j++;
+	      tmp = tmp->next;
+	    }
+	}
+    }
+  printf("}\n");
+}

c9e91ae57333b26504a7d46d8106c113bd3778da
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-07-02 05:54:52 +0000

    Delete hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,37 +1,31 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - delete the whole hash table
- * @ht: the hash table
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: pointer to the hash table
+ * Return: no return
  */
 void hash_table_delete(hash_table_t *ht)
 {
-  unsigned long int i;
-  hash_node_t *tmp;
+	unsigned long int i;
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
 
-  if (ht == NULL)
-    return;
-  for (i = 0; i < ht->size; i++)
-    {
-      if (ht->array[i] != NULL)
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
 	{
-	  printf("First for loop\n");
-	  while (ht->array[i])
-	    {
-	      printf("Freeing\n");
-	      tmp = ht->array[i];
-	      free(tmp->key);
-	      tmp->key = NULL;
-	      free(tmp->value);
-	      tmp->value = NULL;
-	      ht->array[i] = ht->array[i]->next;
-	      free(tmp);
-	      tmp = NULL;
-	    }
+		tmp1 = ht->array[i];
+		while ((tmp2 = tmp1) != NULL)
+		{
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
+		}
 	}
-    }
-  free(ht->array);
-  ht->array = NULL;
-  free(ht);
-  ht = NULL;
-  printf("Leaving\n");
+	free(ht->array);
+	free(ht);
 }

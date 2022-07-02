43c6106e56f81b778ebec3e13269c99e2771b980
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-07-02 05:52:39 +0000

    Association with key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,25 +1,33 @@
 #include "hash_tables.h"
+
 /**
- * *hash_table_get - define function
- * @ht: hash table to lookup
- * @key: key to lookup in the hash table
- * Return: NULL on error, the value of the key on success
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * Return: value of the hash.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index;
-  hash_node_t *tmp;
+	unsigned long int k_index;
+	hash_node_t *tmp;
 
-  index = 0;
-  if (!ht || !key)
-    return (NULL);
-  index = key_index((const unsigned char *)key, ht->size);
-  tmp = ht->array[index];
-  if (tmp)
-    {
-      while (tmp && strncmp(tmp->key, key, strlen(key)) != 0)
-	tmp = tmp->next;
-      return (tmp != NULL ? tmp->value : NULL);
-    }
-  return (NULL);
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[k_index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
 }
